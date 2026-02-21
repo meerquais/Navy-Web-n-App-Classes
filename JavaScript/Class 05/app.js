47af1d0839a9c79463b42f554785d2e0be58a7a1
Meer Quais <meerquais017@gmail.com>
2026-02-21 13:10:12 +0500

    Arrays

---
+++ JavaScript/Class 05/app.js
@@ -0,0 +1,83 @@
+console.log("JavaScript Connected!");
+
+
+// if(condition){
+//     // block of code 
+//     if(condition){
+//         // block of code 
+//     }
+// }
+
+// var age = 10;
+// var hasCNIC = true;
+
+// if(age >= 18){
+//     console.log("Outer Condition");
+    
+//     if(hasCNIC === true){
+//         console.log("vote allowed!");
+        
+//     }
+// }
+
+// var marks =  75;
+
+// if(marks >=50){
+
+//     if(marks >= 80){
+//         console.log("Grade A");
+        
+//     }else{
+//         console.log("Grade B");
+        
+//     }
+
+
+
+
+// }else{
+//     console.log("Failed");
+    
+// }
+
+
+
+// var name = "Meer";
+
+// var name1 = "Meer"
+// var name2 = "Ali"
+// var name3 = "Hafeez"
+// var name4 = "Minhaj"
+// var name5 = "Ayan"
+
+
+// var names = ["Meer", "Ali" , "Hafeez" , "Minhaj" , "Ayan"];
+
+// console.log(names);
+// console.log(names[1]);
+
+
+// var fruits = ["Apple" , "Mango" , "Grapes" , "Banana" , "Strawberry"];
+
+// console.log(fruits);
+// console.log(fruits[4]);
+
+// var details = ["Meer" , 26 , undefined , true , null]
+
+// console.log(details);
+
+// var age = 40;
+
+// age = 20;
+
+
+var fruits = ["Apple" , "Mango" , "Grapes" , "Banana" , "Strawberry"];
+
+console.log(fruits);
+
+
+fruits[0] = "Peach";
+fruits[1] = "Dragonfruit"
+
+
+console.log(fruits);
